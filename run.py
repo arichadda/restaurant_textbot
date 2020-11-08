a3b6f3e47c182ec006d7b46122aea4d28145f7d4
Ari Chadda <ari.chadda@gmail.com>
2020-11-08 01:06:50 -0500

    added virtual enviornment and debugged

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
-from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request
 from Restaurant import Restaurant
 import json
 import requests
 import pprint
-
-
-        
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -22,74 +19,53 @@ def getReply(message):
             curr_str += msg_list[i] + "+"
 
         curr_str += "restaurants"
-
         max_num_returns = msg_list[-1]
+        js_ = requests.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + curr_str + "&radius=5000&key=AIzaSyDYwAFbxHqg9ZnZnl3cvqac32M7zJzRFl8").json()
         
+        parse = []
+        rate = {}
+        rate_val = {}
+        avg_rank = {}
 
-        try:
-            # city = "mexican+restaurant+hanover"
+        for restaurant in js_['results']:
+            
+            try:
+                rating = restaurant['rating']
+                open_ = restaurant['opening_hours']['open_now']
+                name = restaurant['name']
+                user_ratings_count = restaurant['user_ratings_total']
 
-            print(curr_str)
-            js_ = requests.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + curr_str + "&radius=5000&key=AIzaSyDYwAFbxHqg9ZnZnl3cvqac32M7zJzRFl8").json()
-
-            parse = []
-            rate = {}
-            rate_val = {}
-
-            avg_rank = {}
-
-            for restaurant in js_['results']:
+                if rating and name and user_ratings_count and int(user_ratings_count):
+                    curr = Restaurant(name, rating, user_ratings_count, open_)
+                    parse.append(curr)
                 
-                try:
-                    rating = restaurant['rating']
-                    open_ = restaurant['opening_hours']['open_now']
-                    name = restaurant['name']
-                    user_ratings_count = restaurant['user_ratings_total']
+                if curr not in rate.keys():
+                    rate[curr] = user_ratings_count
+                
+                if curr not in rate_val.keys():
+                    rate_val[curr] = rating
 
-                    if open_ and rating and name and user_ratings_count and int(user_ratings_count) > 200:
-                        curr = Restaurant(name, rating, user_ratings_count, open_)
-                        parse.append(curr)
+            except:
+                continue
+        
+        if rate and rate_val:
+            sorted_rate = {k: v for k, v in sorted(rate.items(), key=lambda item: item[1], reverse=True)}
+            sorted_rate_val = {k: v for k, v in sorted(rate_val.items(), key=lambda item: item[1], reverse=True)}
 
-                    if user_ratings_count not in rate:
-                        rate[user_ratings_count] = [curr]
-                    else:
-                        rate[user_ratings_count].append(curr)
+            max = len(list(sorted_rate.keys()))
+            for score in range(len(list(sorted_rate.keys()))):
+                avg_rank[list(sorted_rate.keys())[score]] = (max - score) / 4
                     
-                    if rating not in rate_val:
-                        rate_val[rating] = [curr]
-                    else:
-                        rate_val[rating].append(curr)
+            for rating in range(len(list(sorted_rate_val.keys()))):
+                avg_rank[list(sorted_rate_val.keys())[rating]] += (max - rating) / 2 
 
-                except:
-                    continue
-            
-            print(rate)
-            print(rate_val)
-            if rate and rate_val:
-                sorted_rate = sorted(rate.keys(), reverse=True)
-                sorted_rate_val = sorted(rate_val.keys(), reverse=True)
+            sorted_avg_rank = sorted(avg_rank.items(), key=lambda x:x[1], reverse=True)
 
-
-                for i in range(len(sorted_rate)):
-                    for j in rate[sorted_rate[i]]:
-                        avg_rank[j] = i / 4
-
-                for i in range(len(sorted_rate_val)):
-                    for j in rate_val[sorted_rate_val[i]]:
-                        avg_rank[j] += i / 2
-
-                sorted_avg_rank = sorted(avg_rank.items(), key=lambda x:x[1])
-
-                
-                answer = ""
-                for i in range(int(max_num_returns)):
-                    answer += str(i + 1) + ". "
-                    answer += str(sorted_avg_rank[i][0])
-                    answer += '\n\n'
-
-            
-        except:
-            answer = "We do not have data on " + restaurant
+            answer = ""
+            for i in range(int(max_num_returns)):
+                answer += str(i + 1) + ". "
+                answer += str(sorted_avg_rank[i][0])
+                answer += '\n\n'
 
     else:
         answer = "Please use the format: '[CITY] [STATE ABBREV] [MAX # RESULTS]' for example: 'Hanover NH 5'"
